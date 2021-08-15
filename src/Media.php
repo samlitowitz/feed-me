32a1f447cb963b575e05bc67560ff42f0ed31fb4
Sam Litowitz <samlitowitz@users.noreply.github.com>
2021-08-15 14:25:26 -0400

    Remove type and preserve tests

--- src/Media.php
+++ src/Media.php
@@ -2,11 +2,7 @@
 
 namespace FeedMe;
 
-use FeedMe\Media\Type;
-
 interface Media {
-	// TODO: Improve name `getBinaryData`
 	public function getBinaryData(): string;
 	public function getURI(): string;
-	public function getType(): Type;
 }
